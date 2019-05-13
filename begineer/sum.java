2e00bc1e7ed3b870cc584dfaf9226744049e9635
sanlog9595 <49307730+sanlog9595@users.noreply.github.com>
2019-05-13 15:10:57 +0530

    sum.java

---
+++ begineer/sum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Sum_k_number
+{
+public satic final Scanner cin=new Scanner(system.in);
+public static void main(String args[]args)
+int n=cin.nextInt();
+int m=cin.nextInt();
+int[]arr=new int[n];
+for(int i=0;i<n;i++)
+{
+arr[i]=cin.nextInt();
+}
+int sum=0,i=0;
+while(i<m)
+{
+sum+=arr[i];
+i++;
+}
+System.out.println(sum);
+}
+}
+
